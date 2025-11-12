394d1781afa40dc129d9734096abb9f426eee53d
Mauricioe94 <kusheze94@gmail.com>
2025-11-11 21:07:23 -0300

    primer commit

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Configuración Firebase separada para mejor organización
+const firebaseConfig = {
+    apiKey: "AIzaSyD89yZfOmWYVJphe08Fi3gz9tWlIsv6dE5o",
+    authDomain: "vatos-barbershop.firebaseapp.com",
+    projectId: "vatos-barbershop",
+    storageBucket: "vatos-barbershop.firebasestorage.app",
+    messagingSenderId: "358166347787",
+    appId: "1:358166347787:web:32bd07ceddef274b12c79e"
+};
